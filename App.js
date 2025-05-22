d2dce58bed3c7227b410a5d73bc84cf0a0cc94a9
Laura <160142088+laura-lim@users.noreply.github.com>
2025-05-21 21:40:10 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,22 +1,22 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './src/screens/HomeScreen';
-import DetailScreen from './src/screens/DetailScreen';
-import ProfileScreen from './src/screens/ProfileScreen';
-import LoginScreen from './src/screens/LoginScreen';
+import HomeScreen from './src/screens/HomeScreen.js';
+import DetailScreen from './src/screens/DetailScreen.js';
+import ProfileScreen from './src/screens/ProfileScreen.js';
+import LoginScreen from './src/screens/LoginScreen.js';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <StackNavigator initialRouteName="Home"> 
+      <Stack.Navigator initialRouteName="Home"> 
         <Stack.Screen name = "Home" component={HomeScreen} />
         <Stack.Screen name = "Details" component={DetailScreen} />
         <Stack.Screen name = "Profile" component={ProfileScreen} />
         <Stack.Screen name = "Login" component={LoginScreen} />
-      </StackNavigator>
+      </Stack.Navigator>
     </NavigationContainer>
    
   );
