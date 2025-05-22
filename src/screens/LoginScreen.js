656d07aea15767f767ee171bd8cfc1d098700ce4
Laura <160142088+laura-lim@users.noreply.github.com>
2025-05-21 21:41:12 -0300

    Update LoginScreen.js

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -4,73 +4,45 @@ import {View, Text, Button, StyleSheet, Dimensions, TextInput, useState } from '
 const windowWidht = Dimensions.get('window').width;
 
 export default function LoginScreen ({navigation}) {
-
-    const [login, setLogin] = useState('');
-    const [senha, setSenha] = useState('');
-    const msg = "Bem vindo" + {login};
-
-return (
+    return (
    <View style={styles.container}>
-   <Text style={styles.container}>Login</Text>
-   <TextInput 
-   style={styles.container2}>
-   placeholder = "Email: ",
-   value = {login};
-   onPress= {setLogin};
-   </TextInput>
-   <TextInput 
-   style={styles.container2}>
-   placeholder = "Senha: ",
-   value = {senha};
-   onPress= {setSenha};
-   </TextInput>
-   <Button 
-       title="Logar"       
-       onPress={() => msg}/>
-
+   <Text style={styles.container}>Login Screen</Text>
    <View style={styles.buttonContainer}>
        <Button 
-       title="Go to Home"
-       onPress={()=>  navigation.navigate('Home')}
-       />
-   </View>
-
-   <View style={styles.buttonContainer}>
-       <Button 
-       title="Go to Profile"
-       onPress={()=>  navigation.navigate('Profile')}
+       title="Go to Details"
+       onPress={()=>  navigation.navigate('Details')}
        />
    </View>
    
        <View style={styles.buttonContainer}>
            <Button
-           title="Go to Details"
-           onPress={() => navigation.navigate('Details')}
+           title="Go to profile"
+           onPress={() => navigation.navigate('Profile')}
            />
    </View>
+   
+    <Button 
+          title="Go to Home"
+          onPress={()=>  navigation.navigate('Home')}
+          />
+   
    </View>
     );
    }
-
+   
    const styles = StyleSheet.create({
        container: {
            flex: 1,
            justifyContent: 'center',
            alignItems: 'center',
-           backgroundColor: '##a52a2a',
+           backgroundColor: '#f0f8ff',
        },
-       container2: {
-        fontSize: 12,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '##a52a2a',
-    },
        title: {
            fontSize: 24,
            marginBottom: 20,
        },
        buttonContainer: {
-           backgroundColor: '##a52a2a',
+           backgroundColor: '#add8e6',
            margin: 10,
            width: windowWidht * 0.5,
            borderRadius: 5,
